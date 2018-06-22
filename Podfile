1b5967d19e6a1d36da05abc3fb82137157e25ca6
Keshav Bansal <keshav@intouchapp.com>
2018-06-22 20:25:32 +0530

    pods added

---
+++ Podfile
@@ -0,0 +1,9 @@
+platform :ios, '9.0'
+
+target 'AviatoRoster' do
+  use_frameworks!
+  
+  pod 'RealmSwift'
+  pod 'ObjectMapper'
+
+end
---
+++ Pods/Target Support Files/Pods-AviatoRoster/Pods-AviatoRoster-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_AviatoRoster : NSObject
+@end
+@implementation PodsDummy_Pods_AviatoRoster
+@end
