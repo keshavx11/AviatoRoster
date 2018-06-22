8710017a4c48fb98fed4ce1b13d110ce82c03b97
Keshav Bansal <keshav@intouchapp.com>
2018-06-22 19:50:00 +0530

    Initial Commit

---
+++ AviatoRoster/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  AviatoRoster
+//
+//  Created by Keshav Bansal on 22/06/18.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
